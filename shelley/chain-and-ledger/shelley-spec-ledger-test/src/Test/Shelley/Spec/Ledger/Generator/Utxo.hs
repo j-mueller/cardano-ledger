ee7b565444a94b8972f2e80b9bc6edafeeb2e4c9
Tim Sheard <tim.sheard@IOHK.io>
2021-06-21 16:58:02 -0700

    Added 2 arg scripts, and associated helper functions.
    (pay,stake) script pairs now may have plutus in both parts.
    Added the use of plutus scripts in minting as well.
    Discards are now traced to avoid silence.
    Now we use valueFromList rather than Map.singleton to make Values,
    this avoids the introduction of non-canonical values.
    Added an example of how to profile a property test.

--- shelley/chain-and-ledger/shelley-spec-ledger-test/src/Test/Shelley/Spec/Ledger/Generator/Utxo.hs
+++ shelley/chain-and-ledger/shelley-spec-ledger-test/src/Test/Shelley/Spec/Ledger/Generator/Utxo.hs
@@ -18,6 +18,7 @@ module Test.Shelley.Spec.Ledger.Generator.Utxo
     showBalance,
     getNRandomPairs,
     encodedLen,
+    myDiscard,
   )
 where
 
@@ -115,6 +116,11 @@ import Test.Shelley.Spec.Ledger.Generator.Update (genUpdate)
 import Test.Shelley.Spec.Ledger.Utils (Split (..))
 import Cardano.Ledger.Era(Era)
 import NoThunks.Class()  -- Instances only
+import Debug.Trace(trace)
+
+
+myDiscard :: String -> a
+myDiscard message = trace ("Discard: "++message) discard
 
 import Debug.Trace(trace)
 
@@ -233,7 +239,7 @@ genTx
       let txWits = spendWits ++ wdrlWits ++ certWits ++ updateWits
           scripts = mkScriptWits @era spendScripts (certScripts ++ wdrlScripts)
           mkTxWits' txbody =
-            mkTxWits @era (utxo,txbody,ssHash scriptspace)  ksIndexedPaymentKeys
+            mkTxWits @era (utxo,txbody,(ssHash3 scriptspace,ssHash2 scriptspace))  ksIndexedPaymentKeys
                      ksIndexedStakingKeys txWits scripts (hashAnnotated txbody)
       -------------------------------------------------------------------------
       -- SpendingBalance, Output Addresses (including some Pointer addresses)
@@ -263,8 +269,7 @@ genTx
       -- Occasionally we have a transaction generated with insufficient inputs
       -- to cover the deposits. In this case we discard the test case.
       let enough = (length outputAddrs) <×> (getField @"_minUTxOValue" pparams)
-      !_ <- when (coin spendingBalance < coin enough)
-              (myDiscard ("not enough coin in outputs. needed: "++show enough++", available: "++show spendingBalance) discard)
+      !_ <- when (coin spendingBalance < coin enough) (myDiscard "No inputs left. Utxo.hs")
 
       -------------------------------------------------------------------------
       -- Build a Draft Tx and repeatedly add to Delta until all fees are
@@ -293,7 +298,7 @@ genTx
           scripts' = Map.fromList $ map (\s -> (hashScript @era s, s)) additionalScripts
       -- We add now repeatedly add inputs until the process converges.
       converge
-        (ssHash scriptspace)
+        (ssHash3 scriptspace, ssHash2 scriptspace)
         remainderCoin
         txWits
         (scripts `Map.union` scripts')
@@ -446,9 +451,7 @@ genNextDelta
                 -- If it does happen, It is NOT a test failure, but an inadequacy in the
                 -- testing framework to generate almost-random transactions that always succeed every time.
                 -- Experience suggests that this happens less than 1% of the time, and does not lead to backtracking.
-
-                !_ <- when (null inputs) (myDiscard ("Can't generate a new input, the Utxo is empty") discard)
-
+                !_ <- when (null inputs) (myDiscard "NoMoneyleft Utxo.hs")
                 let newWits =
                       mkTxWits @era
                         (utxo, txBody, scriptinfo)
